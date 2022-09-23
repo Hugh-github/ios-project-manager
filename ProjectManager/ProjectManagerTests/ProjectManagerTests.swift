5b2d86f8452b6c09f8f264a6dfd706ebfdcb5385
MinsuJeon <chso294@naver.com>
2022-09-23 20:33:47 +0900

    feat: 단일 sut

--- ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
+++ ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
@@ -9,38 +9,23 @@
 import XCTest
 
 final class ProjectManagerTests: XCTestCase {
-    var sut: ToDoViewModel!
-    var sut2: DoingViewModel!
-    var countOfToDo: Int?
-    var countOfDoing: Int?
+    var sut: ToDoViewModelSpy!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
 
-        sut = ToDoViewModel(databaseManager: MockLocalDatabaseManager())
-        sut2 = DoingViewModel(databaseManager: MockLocalDatabaseManager())
-        countOfToDo = 0
-        countOfDoing = 0
-
-        sut.toDoData.subscribe { [weak self] _ in
-            self?.countOfToDo! += 1
-        }
-
-        sut2.doingData.subscribe { [weak self] _ in
-            self?.countOfDoing! += 1
-        }
+        sut = ToDoViewModelSpy()
+        sut.callCountOfData = 0
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
 
         sut = nil
-        sut2 = nil
     }
 
     func test_바인딩하는_클로저가_정상적으로_호출되는지_테스트() {
         // given
-        countOfToDo = 0
         let projectUnit = ProjectUnit(
             id: UUID(),
             title: "타이틀",
@@ -51,69 +36,103 @@ final class ProjectManagerTests: XCTestCase {
         let expectation = 1
 
         // when
-        sut.toDoData.value.append(projectUnit)
+        sut.data.value.append(projectUnit)
 
         // then
-        XCTAssertEqual(expectation, countOfToDo)
+        XCTAssertEqual(expectation, sut.callCountOfData)
     }
 
     func test_viewModel이_projectUnit을_정상적으로_코어데이터에_저장하는지_테스트() {
         // given
+        let expectationCalledCount = 1
         let expectationTitle = "테스트"
         let expectationBody = "테스트를 진행합니다"
         let expectationDate = Date()
 
         // when
         sut.addContent(title: expectationTitle, body: expectationBody, date: expectationDate)
-        let result = sut.fetch(0)
 
         // then
-        XCTAssertEqual(expectationTitle, result?.title)
-        XCTAssertEqual(expectationBody, result?.body)
-        XCTAssertEqual(expectationDate, result?.deadLine)
+        XCTAssertEqual(expectationCalledCount, sut.addContentMethodCalled)
+        XCTAssertEqual(expectationTitle, sut.addContentMethodTitleParam)
+        XCTAssertEqual(expectationBody, sut.addContentMethodBodyParam)
+        XCTAssertEqual(expectationDate, sut.addContentMethodDateParam)
     }
 
     func test_viewModel이_코어데이터에_저장된_projectUnit을_정상적으로_삭제하는지_테스트() {
         // given
-        let _ = sut.addContent(title: "타이틀1", body: "바디1", date: Date())
-        let _ = sut.addContent(title: "타이틀2", body: "바디2", date: Date())
         let expectation = 1
+        let expectationCalledCount = 1
+        let expectationIndex = 0
+
+        let firstProjectUnit = ProjectUnit(
+            id: UUID(),
+            title: "타이틀1",
+            body: "바디1",
+            section: sut.identifier,
+            deadLine: Date()
+        )
+        let secondProjectUnit = ProjectUnit(
+            id: UUID(),
+            title: "타이틀2",
+            body: "바디2",
+            section: sut.identifier,
+            deadLine: Date()
+        )
+        sut.data.value = [firstProjectUnit, secondProjectUnit]
 
         // when
-        sut.delete(0)
-        let result = sut.toDoData.value.count
+        sut.delete(expectationIndex)
 
         // then
-        XCTAssertEqual(expectation, result)
+        XCTAssertEqual(expectation, sut.numberOfData)
+        XCTAssertEqual(expectationCalledCount, sut.deleteMethodCalled)
+        XCTAssertEqual(expectationIndex, sut.deleteMethodIndexParam)
     }
 
     func test_viewModel이_코어데이터에_저장된_projectUnit을_정상적으로_업데이트하는지_테스트() {
         // given
-        let _ = sut.addContent(title: "타이틀", body: "바디", date: Date())
+        let expectationCallCount = 1
+        let expectationIndex = 0
         let expectationTitle = "새로운 타이틀"
         let expectationBody = "새로운 바디"
         let expectationDate = Date()
 
-        // when
-        sut.edit(indexPath: 0, title: expectationTitle, body: expectationBody, date: expectationDate)
-        let result = sut.toDoData.value.first
-
-        // then
-        XCTAssertEqual(expectationTitle, result?.title)
-        XCTAssertEqual(expectationBody, result?.body)
-        XCTAssertEqual(expectationDate, result?.deadLine)
-    }
-
-    func test_toDoViewModel의_데이터가_doingViewModel로_정상적으로_이동하는지_테스트() {
-        // given
-        countOfDoing = 0
-        let _ = sut.addContent(title: "타이틀", body: "바디", date: Date())
-        let expectation = 1
+        let projectUnit = ProjectUnit(
+            id: UUID(),
+            title: "타이틀1",
+            body: "바디1",
+            section: sut.identifier,
+            deadLine: Date()
+        )
+        sut.data.value = [projectUnit]
 
         // when
-        sut.change(index: 0, status: "DOING")
+        sut.edit(
+            indexPath: expectationIndex,
+            title: expectationTitle,
+            body: expectationBody,
+            date: expectationDate
+        )
 
         // then
-        XCTAssertEqual(expectation, countOfDoing)
+        XCTAssertEqual(expectationCallCount, sut.editMethodCalled)
+        XCTAssertEqual(expectationIndex, sut.editMethodIndexParam)
+        XCTAssertEqual(expectationTitle, sut.editMethodTitleParam)
+        XCTAssertEqual(expectationBody, sut.editMethodBodyParam)
+        XCTAssertEqual(expectationDate, sut.editMethodDateParam)
     }
+
+//    func test_toDoViewModel의_데이터가_doingViewModel로_정상적으로_이동하는지_테스트() {
+//        // given
+//        countOfDoing = 0
+//        let _ = sut.addContent(title: "타이틀", body: "바디", date: Date())
+//        let expectation = 1
+//
+//        // when
+//        sut.change(index: 0, status: "DOING")
+//
+//        // then
+//        XCTAssertEqual(expectation, countOfDoing)
+//    }
 }
